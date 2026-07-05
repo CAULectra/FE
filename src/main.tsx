496d644ac3087865a190017dad67b91c4814a251
summersurf0452 <tendo0452@gmail.com>
2026-07-05 22:42:20 +0900

    ✨ feat: wireframe MVP 기준으로 app shell 전면 재구축

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 
   import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
+  import AppV2 from "./v2/AppV2.tsx";
   import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+  createRoot(document.getElementById("root")!).render(<AppV2 />);
